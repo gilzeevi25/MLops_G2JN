0a1d9a274529ab3b61f04284af743eb02ba964c3
Gil Zeevi <gilsmol91@gmail.com>
2023-01-07 09:39:48 +0200

    init_params_change

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 from pipeline import G2JN_Pipeline
+
+
+
 def load_boston():
     column_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
     boston_dataset = pd.read_csv("data/housing.csv", header=None, delimiter=r"\s+", names=column_names)
@@ -8,22 +11,61 @@ def load_boston():
     name = "Boston-Housing"
     return  X_bos, y_bos, name
 
-
 def load_motors():
-    motors_dataset = pd.read_csv('data/freMTPL2freq.csv')
-    # Convert Categorical features to Numerical Features
-    for col in ['Area','VehBrand','VehGas','Region']:
-        d = {}
-        for i, val in enumerate(motors_dataset[col].unique()):
-            d[val] = i + 1
-        motors_dataset[col] = motors_dataset[col].apply(lambda x: d[x])
-    # Calculate Frequency
-    motors_dataset['Frequency'] = motors_dataset['ClaimNb'] / motors_dataset['Exposure']
-    X_mot, y_mot = motors_dataset.drop(['IDpol', 'ClaimNb', 'Exposure', 'Frequency'],axis=1), motors_dataset['Frequency']
-    name = "French-Motor-claims"
-    return X_mot, y_mot,name
+  motors_dataset = pd.read_csv('data/freMTPL2freq.csv')#.sample(frac=0.1,random_state=1).reset_index(drop=True)
+  motors_dataset['Frequency'] = motors_dataset['ClaimNb'] / motors_dataset['Exposure']
+  X_mot, y_mot = motors_dataset.drop(['IDpol', 'ClaimNb', 'Exposure', 'Frequency'],axis=1), motors_dataset['Frequency']
+  categorical_columns = X_mot.dtypes[X_mot.dtypes == 'object'].index
+  # Perform one-hot encoding on the categorical columns
+  one_hot_df = pd.get_dummies(X_mot[categorical_columns], dtype=float)
+  X_mot =  (X_mot.drop(categorical_columns,axis=1)).join(one_hot_df).reset_index(drop=True)
+  name = "French-Motor-claims"
+  return X_mot, y_mot,name
+
+def parameters_tuning(X,y,name):
+  params_df = pd.DataFrame()
+  res = G2JN_Pipeline(X, y, name)
+  res.fit(conf_int =95)
+  for thres in [0.5,0.7]:
+    for percenti_t in [25,50,75]:
+      for f_thr in [10,30,95]:
+        for min_in_bin in [5,10,15]:
+          for mutate in [True,False]:
+            if mutate:
+              for frac in [0.05,0.2,0.5]:
+                try:
+                  res.transform( samples_per_bin =30,
+                                        max_bins = 750,
+                                        method='mean',
+                                        threshold=thres,
+                                        percentile_threshold = percenti_t,
+                                        min_amount_samples_in_bin = min_in_bin,
+                                        mutate =mutate,
+                                        frac = frac,
+                                        f_thr = f_thr)  
+                  params_df = pd.concat([params_df,pd.DataFrame(res.parameters,index=[0])]).reset_index(drop=True)
+                  params_df.to_csv('motors_tuning_ALL_CTGAN.csv',index=False)
+                except:
+                  continue
+            else:
+              try:
+                res.transform( samples_per_bin =30,
+                                      max_bins = 750,
+                                      method='mean',
+                                      threshold=thres,
+                                      percentile_threshold = percenti_t,
+                                      min_amount_samples_in_bin = min_in_bin,
+                                      mutate =mutate,
+                                      frac = frac,
+                                      f_thr = f_thr)  
+                params_df = pd.concat([params_df,pd.DataFrame(res.parameters,index=[0])]).reset_index(drop=True)
+                params_df.to_csv('motors_tuning_ALL_CTGAN.csv',index=False)
+              except:
+                continue
+  return 
 
 if __name__ == "__main__":
+    #parameters_tuning(load_motors())
     X_bos, y_bos, name = load_boston()
     print("--------------------------------"*3)
     print("--------------------------------"*3)
@@ -37,5 +79,10 @@ if __name__ == "__main__":
     X_mot, y_mot, name = load_motors()
     pipline_mot = G2JN_Pipeline(X_mot, y_mot, name )
     pipline_mot.fit()
-    pipline_mot.transform()
+    pipline_mot.transform(  max_bins = 750,
+                            threshold=0.7,
+                            min_amount_samples_in_bin = 15,
+                            mutate =True,
+                            frac = 0.2
+                            f_thr = 95)
 
--- pipeline.py
+++ pipeline.py
@@ -3,6 +3,12 @@ import pandas as pd
 from utils.help_func import *
 import xgboost as xgb
 from sklearn.metrics import mean_squared_error,mean_absolute_error
+from sklearn.pipeline import Pipeline
+from sklearn.feature_selection import SelectPercentile, f_regression
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.filterwarnings("ignore")
 
 class G2JN_Pipeline:
     def __init__(
@@ -23,17 +29,33 @@ class G2JN_Pipeline:
         self.y_name = y.name
         self.name = name
         self.xg_reg = xg_reg # baseline model - xgboost
+        self.parameters ={
+          'name':self.name,
+          'org_rmse': None,
+          'imprv_rmse':None,
+          'conf_int': 0,
+          'samples_per_bin': 0,
+          'max_bins': 0,
+          'bin_method': 0,
+          'threshold':0,
+          'percentile_threshold': 0,
+          'min_amount_samples_in_bin' : 0,
+          'mutate': 0,
+          'mutate_frac':0}
 
         
     def fit(self,conf_int = 95,mac_seed= 101):
         print(f"Initializing Pipeline on {self.name} dataset...\n")
         # Data split
         self.splited = split_bin(self.X,self.y)
+        self.parameters['conf_int']= conf_int
         # Initial prediction
         self.xg_reg.fit(self.splited['X_train'],self.splited['y_train'])
         self.preds = self.xg_reg.predict(self.splited['X_test'])
         self.rmse_org = np.sqrt(mean_squared_error(self.splited['y_test'], self.preds))
+        self.parameters['org_rmse'] = self.rmse_org
         self.mae_org = mean_absolute_error(self.splited['y_test'], self.preds)
+        self.parameters['org_mae'] = self.mae_org
         print(f"Initial RMSE: {round(self.rmse_org,2):.2f}\n")
         print(f"Initial MAE: {round(self.mae_org,2):.2f}\n")
 
@@ -47,6 +69,7 @@ class G2JN_Pipeline:
         self.conf_df['diff_prop'] = abs(self.conf_df['upper_bound_pred'] - self.conf_df['lower_bound_pred'])
         self.conf_df['y_pred'] = self.preds
         self.conf_df = self.conf_df.query("lower_bound_pred<=y_pred <= upper_bound_pred").reset_index(drop=True)  # Drop faulted samples
+        print(self.conf_df.head(10).to_string(),"\n")
 
 
     def transform(
@@ -61,9 +84,23 @@ class G2JN_Pipeline:
         gen_len = 1,
         mutate = False,
         frac = 0.05,
-        min_rep = 5
+        min_rep = 5,
+        f_thr = False
 
         ):
+        self.parameters['name']=self.name
+        self.parameters['Gen_Model'] ='TVAE'
+        self.parameters['samples_per_bin'] = samples_per_bin
+        self.parameters['max_bins'] = max_bins
+        self.parameters['bin_method']=method
+        self.parameters['threshold']=threshold
+        self.parameters['percentile_threshold'] = percentile_threshold
+        self.parameters['min_amount_samples_in_bin'] = min_amount_samples_in_bin
+        self.parameters['mutate'] = mutate
+        self.parameters['frac']=frac
+        self.parameters['min_rep']= min_rep
+        self.parameters['f_thr'] = f_thr
+        
 
         # Bin the data
         self.bins_df, _ = assign_bins( # CHECK IF self.splited GETS BINNED ALONG THE WAY TOO... IT DOES ON COLAB BUT NOT necessarily ON CLASS
@@ -95,7 +132,7 @@ class G2JN_Pipeline:
             # Aleatoric uncertainty
         self.bins_df.loc[(self.bins_df.suspected_low_in_data_bins == False) & (self.bins_df.index.isin(uncertainty_bins) ),'uncertainty_type'] = 'Aleatoric'
 
-        print((self.bins_df.sort_values(by = "count_samples",ascending=False).head(50)).to_string(),"\n") #beautify pandas df print?
+        #print((self.bins_df.sort_values(by = "count_samples",ascending=False).head(50)).to_string(),"\n") #beautify pandas df print?
         
         data = pd.concat([self.splited['X_train'],self.splited['y_train']], axis=1).reset_index(drop=True)
         # Apply MEDIAN MUTATION to deal with aleatoric uncertainty - each bin is dealy separately and then re-stack all data together
@@ -123,11 +160,22 @@ class G2JN_Pipeline:
         self.data_new = data_new
         print(f"Total number of samples in new generated data: {data_new.shape[0]}")
 
+        # Insert last pipeline of feature selection
+        last_pipeline = Pipeline([
+        ('feature_selection', SelectPercentile(score_func=f_regression, percentile=f_thr)),
+        ('model', self.xg_reg)])
+        
+        if f_thr == False:
+          last_pipeline = Pipeline([
+                ('model', self.xg_reg)])
+
         # Final prediction
-        self.xg_reg.fit(self.data_new.drop(labels= [self.y_name,'bins'],axis=1),self.data_new.filter([self.y_name]))
-        preds_new = self.xg_reg.predict(self.splited['X_test'])
+        last_pipeline.fit(self.data_new.drop(labels= [self.y_name,'bins'],axis=1),self.data_new.filter([self.y_name]))
+        preds_new = last_pipeline.predict(self.splited['X_test'])
         self.rmse_imprv = np.sqrt(mean_squared_error(self.splited['y_test'], preds_new))
+        self.parameters['imprv_rmse'] = self.rmse_imprv
         self.mae_imprv = mean_absolute_error(self.splited['y_test'], preds_new)
+        self.parameters['imprv_mae'] =  self.mae_imprv
         print("-------------------------------------------------")   
         print(f"Initial RMSE: {round(self.rmse_org,2):.2f}")
         print(f"Improved RMSE: {round(self.rmse_imprv,2):.2f}")
